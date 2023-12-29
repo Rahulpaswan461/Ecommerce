39a07c162e95bdf571ef475d9a06358b79e4ebcb
Rahul Paswan <rahulpaswandoon@gmail.com>
2023-12-29 19:32:07 +0530

    initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './containers/Header';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<ProductListing />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route>404 Not found page</Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useSelector ,useDispatch} from 'react-redux'
+import ProductComponent from './ProductComponent'
+import axios from 'axios'
+import { setProducts } from '../redux/actions/productActions'
+
+
+const ProductListing=()=>{
+      const products=useSelector(state=>state)
+      const dispatch=useDispatch()
+      const fetchProducts= async ()=>{
+              const response = await axios    
+              .get("https://fakestoreapi.com/products")
+              .catch((error)=>{
+                   console.log("Err",error)
+              })
+            dispatch(setProducts(response.data))
+      }
+       useEffect(()=>{
+              fetchProducts()
+       },[])
+       console.log("Product",products)
+        return (
+              <div className="ui grid container">
+                 <ProductComponent/>
+              </div>
+        )
+}
+export default ProductListing
